9b6d95aefca4c64d80f5d442fca9faaef04773aa
Will Hopkins <willothyh@gmail.com>
2022-09-09 04:05:44 -0700

    IDT / CPU interrupts and handlers

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 #![no_std]
 #![no_main]
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(custom_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use custom_os::println;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    custom_os::test_panic_handler(_info)
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello kernel world!\n");
-    panic!();
+
+    custom_os::init();
+
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+
     loop {}
 }
