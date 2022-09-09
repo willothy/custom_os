59886884cb11a05a590e219fccee2cabc0398c56
Will Hopkins <willothyh@gmail.com>
2022-09-09 02:29:47 -0700

    VGA text buffer

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello kernel world!\n");
+    panic!();
+    loop {}
+}
