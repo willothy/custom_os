d5b581c54d2978a249188a192b0130e23bd7f6e6
Will Hopkins <willothyh@gmail.com>
2022-09-09 05:00:18 -0700

    Timer and keyboard interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,20 @@ pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 
+use core::panic::PanicInfo;
+
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
-use core::panic::PanicInfo;
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -44,7 +52,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -69,7 +77,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use custom_os::println;
+use custom_os::{hlt_loop, println};
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
@@ -22,18 +22,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello kernel world!\n");
+    println!("Initializing...");
 
     custom_os::init();
-
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
-    };
 
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
 
-    loop {}
+    hlt_loop();
 }
